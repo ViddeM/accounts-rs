d0b6c6efd1902640c22fcca4052ac85ef014a542
Vidar Magnusson <git@vidarmagnusson.com>
2023-04-15 11:26:03 +0200

    Fix outdated endpoint

--- backend/src/services/create_account_service.rs
+++ backend/src/services/create_account_service.rs
@@ -34,7 +34,7 @@ impl From<sqlx::Error> for CreateAccountError {
     }
 }
 
-const ACTIVATE_ACCOUNT_ENDPOINT: &str = "/api/activate_account";
+const ACTIVATE_ACCOUNT_ENDPOINT: &str = "/api/core/activate_account";
 
 pub async fn create_account(
     config: &State<Config>,
