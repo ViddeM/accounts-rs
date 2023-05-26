132a871c6a44eb3fb34a234102d14356e393092f
Vidar Magnusson <git@vidarmagnusson.com>
2023-05-26 20:56:59 +0200

    Redirect to /me if logged in from /login

--- frontend/src/pages/login/index.tsx
+++ frontend/src/pages/login/index.tsx
@@ -1,21 +1,30 @@
 import CardLayout from "../../layouts/CardLayout";
 import React from "react";
-import {LOGIN_ENDPOINT} from "../../api/Endpoints";
+import { LOGIN_ENDPOINT } from "../../api/Endpoints";
 import Link from "next/link";
 import styles from "./index.module.scss";
+import { useMe } from "../../hooks/useMe";
+import { useRouter } from "next/router";
 
 const Login = () => {
-    return (
-        <CardLayout>
-            <div className={`card mainCard`}>
-                <h2>User is unauthorized</h2>
+  const { me } = useMe();
+  const router = useRouter();
 
-                <Link href={LOGIN_ENDPOINT} className={styles.loginLink}>
-                    Login
-                </Link>
-            </div>
-        </CardLayout>
-    );
+  if (typeof window !== "undefined" && me) {
+    router.push("/me");
+  }
+
+  return (
+    <CardLayout>
+      <div className={`card mainCard`}>
+        <h2>User is unauthorized</h2>
+
+        <Link href={LOGIN_ENDPOINT} className={styles.loginLink}>
+          Login
+        </Link>
+      </div>
+    </CardLayout>
+  );
 };
 
 export default Login;
--- frontend/src/pages/me/index.tsx
+++ frontend/src/pages/me/index.tsx
@@ -1,34 +1,37 @@
 import React from "react";
-import {useMe} from "../../hooks/useMe";
+import { useMe } from "../../hooks/useMe";
 import CardLayout from "../../layouts/CardLayout";
 import styles from "./index.module.scss";
 import Link from "next/link";
 
 const Me = () => {
-    let {me} = useMe();
+  const { me } = useMe();
 
-    return (
-        <CardLayout>
-            <div className={`card mainCard`}>
-                <h2>
-                    {/*TODO: fix translations*/}
-                    My account
-                </h2>
+  return (
+    <CardLayout>
+      <div className={`card mainCard`}>
+        <h2>
+          {/*TODO: fix translations*/}
+          My account
+        </h2>
 
-                <div className={styles.myInfo}>
-                    <div className={styles.infoRow}>
-                        <p>Name:</p> {me?.firstName} {me?.lastName}
-                    </div>
-                    <div className={styles.infoRow}>
-                        <p>Email:</p> {me?.email}
-                    </div>
-                </div>
-                <Link href={"/api/core/forgot_password"} className={styles.resetPasswordLink}>
-                    Reset password
-                </Link>
-            </div>
-        </CardLayout>
-    );
+        <div className={styles.myInfo}>
+          <div className={styles.infoRow}>
+            <p>Name:</p> {me?.firstName} {me?.lastName}
+          </div>
+          <div className={styles.infoRow}>
+            <p>Email:</p> {me?.email}
+          </div>
+        </div>
+        <Link
+          href={"/api/core/forgot_password"}
+          className={styles.resetPasswordLink}
+        >
+          Reset password
+        </Link>
+      </div>
+    </CardLayout>
+  );
 };
 
 export default Me;
